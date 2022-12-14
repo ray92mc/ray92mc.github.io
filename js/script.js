0a89adc825798c368a6677894de9dd7df3143c65
ray92mc <54141956+ray92mc@users.noreply.github.com>
2022-12-14 08:48:51 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// add class navbarDark on navbar scroll
+const header = document.querySelector('.navbar');
+
+window.onscroll = function() {
+    var top = window.scrollY;
+    if(top >=100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+
+function sendMail(){
+    var params = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        subject: document.getElementById('subject').value,
+        message: document.getElementById('message').value
+    };
+
+    const emailServiceId = "service_ajir7mo";
+    const templateId = "template_i7a7bp9";
+
+    emailjs.send(emailServiceId, templateId, params).then(res =>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("subject").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Message sent!");
+    }).catch(err=>console.log(err));
+}
+
+
